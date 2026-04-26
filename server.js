4c0e00d6a11a707921d89155415c523b86026102
Navid <nh222yr@student.lnu.se>
2026-04-26 04:12:04 +0200

    fix: correct password format for WT1 API

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/auth/google/callback', async (req, res) => {
     const { email, name } = profileRes.data
 
     const API = process.env.WT1_API_URL
-    const password = `oauth_${email.split('@')[0]}_2026`
+    const password = `oauth_${email.split('@')[0]}1212`
     let jwt
 
     try {
